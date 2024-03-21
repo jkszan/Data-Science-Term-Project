72c4bedf28d5bf5a8d04632e97c37b3a255e10be
MBlomqvist <blomqvist.malte@telia.com>
2024-03-21 16:58:16 -0400

    Year check on stations

--- src/nearest-station/src/main.rs
+++ src/nearest-station/src/main.rs
@@ -24,6 +24,7 @@ fn main() -> Result<(), String> {
     let mut hotspot_data = CsvReader::from_path("../../../firedata_no_station.csv")
         .unwrap()
         .infer_schema(None)
+        .with_try_parse_dates(true)
         .has_header(true)
         .low_memory(true)
         .finish()
@@ -48,11 +49,29 @@ fn main() -> Result<(), String> {
         .str()
         .unwrap();
 
-    let station_ids = weather_stations
+    let station_ids: Vec<(Option<i64>, (Option<i64>, Option<i64>))> = weather_stations
         .column("Station ID")
         .unwrap()
         .i64()
-        .unwrap().to_vec();
+        .unwrap()
+        .iter()
+        .zip(
+            weather_stations
+                .column("First Year")
+                .unwrap()
+                .i64()
+                .unwrap()
+                .iter()
+                .zip(
+                    weather_stations
+                        .column("Last Year")
+                        .unwrap()
+                        .i64()
+                        .unwrap()
+                        .iter(),
+                ),
+        )
+        .collect();
 
     let points: Vec<Point> = latl
         .iter()
@@ -69,29 +88,55 @@ fn main() -> Result<(), String> {
             } else {
                 None
             }
-        }).filter(|f| f.is_some()).map(|f| f.unwrap())
+        })
+        .filter(|f| f.is_some())
+        .map(|f| f.unwrap())
         .collect();
+
+    let fire_years = hotspot_data.column("Date")
+        .unwrap()
+        .year()
+        .unwrap();
 
     let map = Builder::default().build(points, station_ids);
     let mut search = Search::default();
 
+    if let (Ok(Ok(latcol)), Ok(Ok(loncol))) = (
+        hotspot_data.column("FireLatitude").map(|col| col.f64()),
+        hotspot_data.column("FireLongitude").map(|col| col.f64()),
+    ) {
+        let points: Vec<Option<i64>> = fire_years.iter().zip(latcol
+            .iter()
+            .zip(loncol.iter()))
+            .map(|(yearopt,(latopt, lonopt))| {
+                if let (Some(year),Some(lat), Some(lon)) = (yearopt,latopt, lonopt) {
+                    let mut closestid = None;
+                    let mut s = map.search(&Point(lat, lon), &mut search);
+                    while let Some(res) = s.next(){
+                        if let (Some(id),(Some(firstyear),Some(lastyear))) = res.value{
+                            if *firstyear < (year as i64) && (year as i64) < *lastyear{
+                                closestid = Some(*id);
+                                break;
+                            } 
+                        }
+                    }
+                    closestid
+                } else {
+                    None
+                }
+            })
+            .collect();
 
-    if let (Ok(Ok(latcol)),Ok(Ok(loncol))) = (hotspot_data.column("FireLatitude").map(|col| col.f64()),hotspot_data.column("FireLongitude").map(|col| col.f64())){
-        let points: Vec<Option<i64>> = latcol.iter().zip(loncol.iter()).map(|(latopt, lonopt)| if let (Some(lat),Some(lon)) = (latopt,lonopt){
-            map.search(&Point(lat,lon),&mut search).next().map(|val| val.value.unwrap())
-        }else{None}
-        ).collect();
-
-        let hotspot_data = hotspot_data.with_column(Series::new("ClosestStationID",points)).unwrap();
+        let hotspot_data = hotspot_data
+            .with_column(Series::new("ClosestStationID", points))
+            .unwrap();
         println!("{}", hotspot_data);
         let path = "../../../firedata_station.csv";
         if let Ok(new_firestations_file) = File::create(path) {
             let writer = CsvWriter::new(new_firestations_file);
             writer.include_header(true).finish(hotspot_data).unwrap();
             println!("firedata_station mapping written to {}", path);
-
         }
-
-    } 
+    }
     Ok(())
 }
